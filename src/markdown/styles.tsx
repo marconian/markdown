a5afb3ef057c7bb39467504225badffe62c86f84
Marco Flier <marco.flier@tauw.com>
2025-11-05 07:25:52 +0100

    feat: integrate KaTeX styles for improved math rendering in Markdown

--- src/markdown/styles.tsx
+++ src/markdown/styles.tsx
@@ -1,9 +1,13 @@
 import { Global, css } from '@emotion/react';
 import githubLightTheme from 'highlight.js/styles/github.css?inline';
 import githubDarkTheme from 'highlight.js/styles/github-dark.css?inline';
+import katexTheme from 'katex/dist/katex.min.css?inline';
+
 
 const baseStyles = css`
     ${githubLightTheme}
+
+    ${katexTheme}
 
     .markdown-paper {
         .markdown-img-download {
